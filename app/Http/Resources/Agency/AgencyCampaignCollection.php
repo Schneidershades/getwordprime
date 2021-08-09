58438111a61d7d955d737cb703a1cd11cd04aa6e
Schneider <schneidershades@gmail.com>
2021-08-09 11:01:36 +0100

    collections

--- app/Http/Resources/Agency/AgencyCampaignCollection.php
+++ app/Http/Resources/Agency/AgencyCampaignCollection.php
@@ -14,6 +14,8 @@ class AgencyCampaignCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => AgencyCampaignResource::collection($this->collection),
+        ];
     }
 }
