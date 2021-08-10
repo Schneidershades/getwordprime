073195a65bce6468dff0e48cb327bbca709933a1
Schneider <schneidershades@gmail.com>
2021-08-10 06:31:38 +0100

    annotation agency

--- app/Http/Controllers/Api/User/AgencyController.php
+++ app/Http/Controllers/Api/User/AgencyController.php
@@ -8,26 +8,209 @@ use App\Models\Agency;
 
 class AgencyController extends Controller
 {
+    /**
+    * @OA\Get(
+    *      path="/api/v1/agencies",
+    *      operationId="userAgencies",
+    *      tags={"agencies"},
+    *      summary="Get All User Agencies",
+    *      description="Get All User Agencies",
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function index()
     {
         $this->showAll(auth()->user()->agencies);
     }
 
+    /**
+    * @OA\Post(
+    *      path="/api/v1/agencies",
+    *      operationId="postAgencies",
+    *      tags={"agency"},
+    *      summary="Post New Agencies",
+    *      description="Post New Agencies",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          @OA\JsonContent(ref="#/components/schemas/AgencyCreateFormRequest")
+    *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function store(Request $request)
     {
         return $this->showOne(auth()->user()->agencies()->create($request->validated()));
     }
 
+    /**
+    * @OA\Get(
+    *      path="/api/v1/agencies/{id}",
+    *      operationId="showAgency",
+    *      tags={"agency"},
+    *      summary="Show an agency",
+    *      description="Show an agency",
+    *      
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Agency ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function show(Agency $agency)
     {
         return $this->showOne(Agency::findOrFail($id));
     }
-
+    /**
+    * @OA\PUT(
+    *      path="/api/v1/agencies/{id}",
+    *      operationId="updateAgency",
+    *      tags={"artisan"},
+    *      summary="Update an agency",
+    *      description="Update an agency",
+    *      
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Agency ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+    *      @OA\RequestBody(
+    *          required=true,
+    *          @OA\JsonContent(ref="#/components/schemas/AgencyUpdateFormRequest")
+    *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function update(AgencyUpdateFormRequest $request, Agency $agency)
     {
         return $this->showOne($agency->update($request->validated()));
     }
 
+     /**
+    * @OA\Delete(
+    *      path="/api/v1/agencies/{id}",
+    *      operationId="deleteAgency",
+    *      tags={"artisan"},
+    *      summary="Delete an agency",
+    *      description="Delete an agency",
+    *      
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Agency ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
     public function destroy(Agency $agency)
     {
         $agency->delete();
