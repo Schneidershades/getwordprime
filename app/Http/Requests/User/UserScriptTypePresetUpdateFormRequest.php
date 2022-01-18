76b3ff4acf3ba9d0465aa7933259d3e90ce04aa0
Schneider <schneidershades@gmail.com>
2022-01-18 21:17:24 +0100

    git push

--- app/Http/Requests/User/UserScriptTypePresetUpdateFormRequest.php
+++ app/Http/Requests/User/UserScriptTypePresetUpdateFormRequest.php
@@ -70,8 +70,8 @@ class UserScriptTypePresetUpdateFormRequest extends FormRequest
     {
         return [
             'answer' => 'required|string',
-            'script_type_preset_id' => 'required|int|exists:script_type_presets,id',
-            'script_type_id' => 'required|int|exists:script_types,id',
+            // 'script_type_preset_id' => 'required|int|exists:script_type_presets,id',
+            // 'script_type_id' => 'required|int|exists:script_types,id',
         ];
     }
 }
