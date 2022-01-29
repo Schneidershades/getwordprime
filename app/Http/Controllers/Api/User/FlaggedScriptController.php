e26194ae39921382cf4e353e6f3640360da8e1c6
Schneider <schneidershades@gmail.com>
2022-01-29 04:18:12 +0100

    git push

--- app/Http/Controllers/Api/User/FlaggedScriptController.php
+++ app/Http/Controllers/Api/User/FlaggedScriptController.php
@@ -40,6 +40,7 @@ class FlaggedScriptController extends Controller
     */
     public function index()
     {
+        return(auth()->user()->flaggedScripts);
         return $this->showAll(auth()->user()->flaggedScripts);
     }
 
