513497b015acf032bbacaeefad07af206a09c67d
Schneider <schneidershades@gmail.com>
2022-03-13 12:55:26 +0100

    creating cart observers and testing checkout

--- app/Models/ScriptResponse.php
+++ app/Models/ScriptResponse.php
@@ -6,13 +6,18 @@ use App\Models\Script;
 use App\Models\FlaggedScript;
 use App\Models\FavoriteScript;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Resources\Script\ScriptResponseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Http\Resources\Script\ScriptResponseCollection;
 
 class ScriptResponse extends Model
 {
     use HasFactory;
     
     protected $guarded = [];
+
+    public $oneItem = ScriptResponseResource::class;
+    public $allItems = ScriptResponseCollection::class;
 
     public function favorite()
     {
