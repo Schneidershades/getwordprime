e9771529cffde7cf02609e838750e0d5615538c3
Schneider <schneidershades@gmail.com>
2022-04-12 01:16:51 +0100

    add usage

--- app/Models/ScriptType.php
+++ app/Models/ScriptType.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Media;
+use App\Models\ScriptResponse;
+use App\Models\ScriptTypePreset;
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Resources\Script\ScriptTypeResource;
 use App\Http\Resources\Script\ScriptTypeCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ScriptType extends Model
 {
