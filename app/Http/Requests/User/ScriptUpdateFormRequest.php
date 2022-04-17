828573d674996da80f2a0a8d8aea86bc0a15ea00
Schneider <schneidershades@gmail.com>
2022-04-17 16:36:38 +0100

    script response update

--- app/Http/Requests/User/ScriptUpdateFormRequest.php
+++ app/Http/Requests/User/ScriptUpdateFormRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
-/**
+
 /**
  * @OA\Schema(
  *      title="Script Update Form Request Fields",
@@ -14,40 +14,17 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 class ScriptUpdateFormRequest extends FormRequest
 {
+    
     /**
      * @OA\Property(
-     *      title="script content",
-     *      description="content of the script",
+     *      title="script text",
+     *      description="text of the script",
      *      example="Info Limited"
      * )
      *
      * @var string
      */
-    public $content;
-
-    /**
-     * @OA\Property(
-     *      title="user script_type id",
-     *      description="Initial related script_type of the id",
-     *      example="1"
-     * )
-     *
-     * @var int
-     */
-    public $script_type_id;
-
-
-    /**
-     * @OA\Property(
-     *      title="user campaign id",
-     *      description="Initial related campaign of the id",
-     *      example="1"
-     * )
-     *
-     * @var int
-     */
-    public $campaign_id;
-    
+    public $text;
 
     /**
      * Determine if the user is authorized to make this request.
@@ -67,9 +44,7 @@ class ScriptUpdateFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => 'required|string',
-            'script_type_id' => 'required|int',
-            'campaign_id' => 'required|int',
+            'text' => 'required|string',
         ];
     }
 }
