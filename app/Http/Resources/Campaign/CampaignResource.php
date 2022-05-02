b972370b01fd56324a522c6948cbe800d661f861
Schneider <schneidershades@gmail.com>
2022-05-02 18:32:19 +0100

    nig

--- app/Http/Resources/Campaign/CampaignResource.php
+++ app/Http/Resources/Campaign/CampaignResource.php
@@ -19,7 +19,7 @@ class CampaignResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'scripts_count' => $this->scripts->count(),
+            'scripts_count' => $this->agencies->count(),
             'scripts' => ScriptResponseResource::collection($this->scriptResponses),
             // 'scripts' => ScriptResource::collection($this->scripts),
             'created_at' => $this->created_at,
