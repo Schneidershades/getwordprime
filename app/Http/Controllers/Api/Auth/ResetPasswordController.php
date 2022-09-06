f9be7f74440f5e78e31dd7e9666ce328e074a05e
Schneider <schneidershades@gmail.com>
2022-09-06 04:22:28 +0100

    forgot password

--- app/Http/Controllers/Api/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api/Auth/ResetPasswordController.php
@@ -3,45 +3,68 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-Use Illuminate\Http\Request;
+use App\Http\Requests\Auth\ResetPasswordRequest;
+use App\Models\User;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ResetPasswordController extends Controller
 {
     use ResetsPasswords;
 
-     /**
-    * @OA\Post(
-    *      path="/api/v1/user/password/reset",
-    *      operationId="resetPasswordFromToken",
-    *      tags={"authentication"},
-    *      summary="reset a registered user password",
-    *      description="Returns a registered user reset email",
-    *      @OA\RequestBody(
-    *          required=true,
-    *          @OA\JsonContent(ref="#/components/schemas/ResetPasswordRequest")
-    *      ),
-    *      @OA\Response(
-    *          response=200,
-    *          description="Successful signin",
-    *          @OA\MediaType(
-    *             mediaType="application/json",
-    *         ),
-    *       ),
-    *      @OA\Response(
-    *          response=400,
-    *          description="Bad Request"
-    *      ),
-    *      @OA\Response(
-    *          response=401,
-    *          description="Unauthenticated",
-    *      ),
-    *      @OA\Response(
-    *          response=403,
-    *          description="Forbidden"
-    *      ),
-    * )
-    */
+    /**
+     * @OA\Post(
+     *      path="/api/v1/user/password/reset",
+     *      operationId="resetPasswordFromToken",
+     *      tags={"Authentication"},
+     *      summary="reset a registered user password",
+     *      description="Returns a registered user reset email",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/ResetPasswordRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful signin",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *         ),
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * )
+     */
+    public function reset(ResetPasswordRequest $request)
+    {
+        if ($request->validated()) {
+            $updatePassword = DB::table('password_resets')
+                                ->where([
+                                    'email' => $request['email'],
+                                    'token' => $request['token'],
+                                ])->first();
+
+            if (! $updatePassword) {
+                return $this->errorResponse('Invalid Token', 422);
+            }
+
+            User::where('email', $request->email)->update(['password' => bcrypt($request->password)]);
+
+            DB::table('password_resets')->where(['email' => $request->email])->delete();
+
+            return $this->showMessage('Your password has been changed! . Please login with your email and new password', 200);
+        }
+    }
 
     protected function sendResetResponse(Request $request, $response)
     {
