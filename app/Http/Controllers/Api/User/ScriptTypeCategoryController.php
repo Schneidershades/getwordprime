9126f47567476027658c707891f5b05365b5de45
Schneider <schneidershades@gmail.com>
2022-09-25 13:13:05 +0100

    script type category

---
+++ app/Http/Controllers/Api/User/ScriptTypeCategoryController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\ScriptTypeCategory;
+
+class ScriptTypeCategoryController extends Controller
+{
+    /**
+    * @OA\Get(
+    *      path="/api/v1/script-type-categories",
+    *      operationId="allScriptTypes",
+    *      tags={"user"},
+    *      summary="Get Script-type",
+    *      description="Get script-types",
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
+    public function index()
+    {
+        return $this->showAll(ScriptTypeCategory::all());
+    }
+
+    /**
+    * @OA\Get(
+    *      path="/api/v1/script-type-categories/{id}",
+    *      operationId="showScriptType",
+    *      tags={"user"},
+    *      summary="Show script type",
+    *      description="Show script type",
+    *      @OA\Parameter(
+    *          name="id",
+    *          description="Script Type ID",
+    *          required=true,
+    *          in="path",
+    *          @OA\Schema(
+    *              type="integer"
+    *          )
+    *      ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful signin",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    *      @OA\Response(
+    *          response=400,
+    *          description="Bad Request"
+    *      ),
+    *      @OA\Response(
+    *          response=401,
+    *          description="unauthenticated",
+    *      ),
+    *      @OA\Response(
+    *          response=403,
+    *          description="Forbidden"
+    *      ),
+    *      security={ {"bearerAuth": {}} },
+    * )
+    */
+    public function show(ScriptTypeCategory $scriptType)
+    {
+        return $this->showOne($scriptType);
+    }
+
+}
