fffaa272b589d5bc5db9fb871f51e7445a373baa
Schneider <schneidershades@gmail.com>
2022-09-26 02:23:53 +0100

    scripts count

--- app/Http/Resources/Script/ScriptTypeCategoryResource.php
+++ app/Http/Resources/Script/ScriptTypeCategoryResource.php
@@ -11,7 +11,7 @@ class ScriptTypeCategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            // 'scriptTypes_count' => $this->scriptTypes_count,
+            'scripts_count' => $this->scripts_count,
             'scriptTypes' => ScriptTypeResource::collection($this->whenLoaded('scriptTypes')),
         ];
     }
