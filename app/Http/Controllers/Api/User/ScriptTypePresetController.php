91c8c3f0ab5fcfb6988d5a7cb8f23d24d119b6fe
Schneider <schneidershades@gmail.com>
2022-10-26 18:32:40 +0100

    check items

--- app/Http/Controllers/Api/User/ScriptTypePresetController.php
+++ app/Http/Controllers/Api/User/ScriptTypePresetController.php
@@ -40,4 +40,49 @@ class ScriptTypePresetController extends Controller
     {
         return $this->showAll(ScriptTypePreset::latest()->get());
     }
+
+     /**
+     * @OA\Get(
+     *      path="/api/v1/script-type-presets/{id}",
+     *      operationId="showScriptTypeUserPromptAnswer",
+     *      tags={"user"},
+     *      summary="Show an answer",
+     *      description="Show an answer",
+     *
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Script type preset ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful signin",
+     *          @OA\MediaType(
+     *             mediaType="application/json",
+     *         ),
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      security={ {"bearerAuth": {}} },
+     * )
+     */
+    public function show()
+    {
+        return $this->showAll(ScriptTypePreset::latest()->get());
+    }
 }
