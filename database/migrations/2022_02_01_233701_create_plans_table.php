bdde93b4c5092e08d2edc4e55925821f595688ba
Schneider <schneidershades@gmail.com>
2022-10-30 08:11:08 +0100

    plans

--- database/migrations/2022_02_01_233701_create_plans_table.php
+++ database/migrations/2022_02_01_233701_create_plans_table.php
@@ -18,8 +18,12 @@ return new class extends Migration
             $table->string('name');
             $table->string('type')->nullable();
             $table->string('role')->nullable();
+            $table->string('next_suggested_plan')->nullable();
             $table->boolean('teams')->default(false);
+            $table->boolean('trial')->default(false);
             $table->string('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('full_description')->nullable();
             $table->boolean('discount_applied')->default(false);
             $table->string('discount_mode')->nullable();
             $table->float('amount')->default(0);
